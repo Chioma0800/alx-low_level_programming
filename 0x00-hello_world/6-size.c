c09d203bc08b92396087a7aaa8802db49c2facd7
chioma0800 <nkemijomachioma@gmail.com>
2023-10-08 17:22:08 -0700

    lines

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h >
+/**
+ * main function
+ *
+ * Return: O (success)
+ */
+int main (void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	printf (prints the size of various types char) : % lu byte (s)"
