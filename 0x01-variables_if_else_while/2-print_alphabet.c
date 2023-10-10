2700bc5c577ba8e9d5fe423c1559c57569d4409d
chioma0800 <nkemijomachioma@gmail.com>
2023-10-09 18:58:57 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h >
+#include <ctyrme.h >
+/**
+ * main entery point
+ *
+ * Return always 0 (success)
+ */
+int main (void)
+{
+	int lowercase- "a" ;
+	while (lowercase <-"z")
+	{
+		put char (lowercase) ;
+		lowercase,-1;
+	}
+	putchar ( %\n") ;
+	return (0);
