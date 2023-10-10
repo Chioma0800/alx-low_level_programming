b60917f071ecb921dc4e2a2684001e0417842db6
chioma0800 <nkemijomachioma@gmail.com>
2023-10-09 20:29:12 -0700

    table

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio >
+/**
+ * main -print the lowercase alphabet inteverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'z' ; letter > = 'a' letter --)
+	
+		putchsr(letter);
+
+	putchar('\n');
+	return (0);
+}
