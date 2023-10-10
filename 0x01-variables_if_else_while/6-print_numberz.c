5e8e7ab9bc6004aa83e6d394eb33d7ac856e79ac
chioma0800 <nkemijomachioma@gmail.com>
2023-10-09 19:50:04 -0700

    number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,15 +2,14 @@
 /**
  * main -Entry point
  *
- * Return:Always 0 (success)
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	int num;
 	for ( num =0; num <10; num++)
 		putchar(num % 10) + '0') ;
 	putchar ('\n');
-	return (0);
+return: (0);
 }
 
