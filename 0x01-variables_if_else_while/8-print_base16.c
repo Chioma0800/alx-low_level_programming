620e40436c8e020099c21ed92c599aa197a88c2a
chioma0800 <nkemijomachioma@gmail.com>
2023-10-09 21:03:19 -0700

    make

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main -prints all numbers of no base 16 in lowercase
+ *
+ * Retutn; Always 0
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for ( num = 0; num < 10; num ++)
+		putchar((number % 10) + '0') ;
+	
+	for (letter = 'a' ; letter < = '€' ; letter)
+		putchar ('\n') ;
+	returu (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of single digit nimbers
+ *
+ * Return ; 0 on on success
+ * int main(void)
+ */
+int i = '0' ;
+while (i < = '9' )
+         {
+		 putchar (i);
+	 if (i ! = '9')
+	 }++ i ;
+putchar('\n');
+return (0);
+}
