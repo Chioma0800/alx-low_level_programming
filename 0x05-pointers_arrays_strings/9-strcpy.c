5fca3e46ef4455e98de2299d2587365b6515b581
chioma0800 <nkemijomachioma@gmail.com>
2023-10-17 07:02:18 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,8 +8,8 @@
 char *_strcpy(char *dest, char *src)
 
 {
-        int l = 0;
-        int x = 0;
+        intl = 0;
+        intx = 0;
         while (*(src + l) != '\0')
         {
                 l++;
